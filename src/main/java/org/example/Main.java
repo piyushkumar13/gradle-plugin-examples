f48b9c53337dce456ef0b839471ce1b52ed083f6
piyushkumar13 <piyush.k9013@gmail.com>
2025-03-23 20:45:10 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Piyush Kumar.
+ * @since 23/03/25.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
